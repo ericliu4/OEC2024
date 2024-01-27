7bd564aecf2ad6740d091d68a6226ad8bf8a6ea6
KCTaka <kc.takahashi04@gmail.com>
2024-01-27 14:03:45 -0500

    Button Enlarge

--- pages/menu.py
+++ pages/menu.py
@@ -11,6 +11,9 @@ global button_width, button_height
 button_width = 240
 button_height = 45
 
+global button_size_factor
+button_size_factor = 1
+
 def run_menu(game):
     
     global prev_mouse_state
@@ -26,15 +29,15 @@ def run_menu(game):
     mouse_state = get_mouse_state(pos)
     
     # Fill background
-    bg = pygame.image.load('images/bg.jpg')
+    bg = pygame.image.load('images/bg2.png')
     bg = pygame.transform.scale(bg, (game.width, game.height))
     screen.blit(bg, (0, 0))
 
     # Draw title
     #title_text = game.fonts['title'].render('Dyslexia Disco', True, 'black')
     #title_text_rect = title_text.get_rect(center=(game.width/2, 50))
-    title_text = pygame.image.load('images/title.png')
-    title_text_rect = (game.width/2-500//2, 50)
+    title_text = pygame.image.load('images/mario_title.png')
+    title_text_rect = (game.width/2-616//2, 50)
     screen.blit(title_text, title_text_rect)
     
     # Cool Sprite Animation
@@ -48,7 +51,7 @@ def run_menu(game):
     cropped_region = (x_pos, y_pos, mario_width//11, mario_height//11)
     cropped_image = mario_animation.subsurface(cropped_region)
     cropped_image = pygame.transform.scale(cropped_image, (100,100))
-    screen.blit(cropped_image, (200, 200))
+    screen.blit(cropped_image, (150, 400))
     if width_state//speed_factor <= 6:
         width_state += 1
     else:
@@ -68,16 +71,41 @@ def run_menu(game):
     elif prev_mouse_state and mouse_state == 0:
         pygame.mouse.set_cursor(pygame.SYSTEM_CURSOR_ARROW)
     # Play button
-    play_button_image = pygame.image.load('images/-PLAY.png')
-    screen.blit(play_button_image, (game.width/2 - button_width/2, 200))
+    
+    global button_size_factor
+    max_factor = 1.1
+    if mouse_state == 1:
+        play_button_image = pygame.image.load('images/-PLAY.png')
+        play_button_image = pygame.transform.scale(play_button_image, (button_width*button_size_factor, button_height*button_size_factor))
+        screen.blit(play_button_image, (game.width/2 - button_width*button_size_factor/2, 200))
+        if button_size_factor < max_factor:
+            button_size_factor += 0.01
+        else:
+            button_size_factor = max_factor
+    else:
+        play_button_image = pygame.image.load('images/-PLAY.png')
+        screen.blit(play_button_image, (game.width/2 - button_width/2, 200))
+        
     #screen.fill(play_button_color, (game.width/2 - button_width/2, 200, button_width, button_height))
     #button_text = game.fonts['pt35'].render('Play', True, button_text_color)
     #button_text_rect = button_text.get_rect(center=(game.width/2, 200 + button_height/2))
     #screen.blit(button_text, button_text_rect)
 
     # setting button
-    settings_button_image = pygame.image.load('images/-SETTINGS.png')
-    screen.blit(settings_button_image, (game.width/2 - button_width/2, 300))
+    if mouse_state == 2:
+        settings_button_image = pygame.image.load('images/-SETTINGS.png')
+        settings_button_image = pygame.transform.scale(settings_button_image, (button_width*button_size_factor, button_height*button_size_factor))
+        screen.blit(settings_button_image, (game.width/2 - button_width*button_size_factor/2, 300))
+        if button_size_factor < max_factor:
+            button_size_factor += 0.01
+        else:
+            button_size_factor = max_factor
+    else:
+        settings_button_image = pygame.image.load('images/-SETTINGS.png')
+        screen.blit(settings_button_image, (game.width/2 - button_width/2, 300))
+    
+    if mouse_state == 0:
+        button_size_factor = 1
     #screen.fill(settings_button_color, (game.width/2 - button_width/4, 320, button_width/2, button_height/2))
     #button_text = game.fonts['pt20'].render('Setting', True, button_text_color)
     #button_text_rect = button_text.get_rect(center=(game.width/2, 300 + button_height/2))
